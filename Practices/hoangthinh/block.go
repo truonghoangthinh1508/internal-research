30072fdbf201d2e236d8f87a5c6903b7b86528f5
truonghoangthinh1508 <49168123+truonghoangthinh1508@users.noreply.github.com>
2019-05-22 22:28:29 +0700

    Add files via upload

---
+++ Practices/hoangthinh/block.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "internal-research/Practices/phuocthanh/connection"
+)
+
+func main() {
+    connection.Listen ("localhost:8686")
+    //connection.Connect ("192.168.1.1:8888")
+}
